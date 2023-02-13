167f9a7d5c674a855b46423aacab10277ddeae96
Alan Timbó <alancorreiatimbo@gmail.com>
2023-02-13 15:51:19 -0300

    fix: existing cpf conflict in client update

--- src/middlewares/customers.middleware.js
+++ src/middlewares/customers.middleware.js
@@ -14,18 +14,18 @@ export async function customersValidation(req, res, next) {
 
   try {
     const cpfExist = await db.query("SELECT * FROM customers WHERE cpf=$1", [
-        customer.cpf,
-      ]);
-  
-      if (cpfExist.rowCount > 0) {
-        return res
-          .status(409)
-          .send("This customer already exists. Please try another cpf.");
-      }
-  
-      res.locals.customer = customer;
-  
-      next();
+      customer.cpf,
+    ]);
+
+    if (cpfExist.rowCount > 0 && cpfExist.rows[0].id !== Number(req.params.id)) {
+      return res
+        .status(409)
+        .send("This customer already exists. Please try another cpf.");
+    }
+
+    res.locals.customer = customer;
+
+    next();
   } catch (err) {
     return res.status(500).send(err.message);
   }
