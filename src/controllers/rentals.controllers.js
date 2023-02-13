bb94365e1aea08e3a55b641821a034e0822a0af8
Alan Timbó <alancorreiatimbo@gmail.com>
2023-02-13 17:49:22 -0300

    feat: rentals finding controller

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -23,7 +23,40 @@ export async function createRental(req, res) {
   }
 }
 
-export async function findRentals(req, res) {}
+export async function findRentals(req, res) {
+  try {
+    const rentals = await db.query(
+      `SELECT rentals.*, customers.id, customers.name, games.id, games.name FROM rentals 
+      JOIN customers ON rentals."customerId" = customers.id
+      JOIN games ON rentals."gameId" = games.id`
+    );
+
+    return res.send(rentals.rows);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+}
+
+[
+  {
+    id: 1,
+    customerId: 1,
+    gameId: 1,
+    rentDate: "2021-06-20",
+    daysRented: 3,
+    returnDate: null, // troca pra uma data quando já devolvido
+    originalPrice: 4500,
+    delayFee: null,
+    customer: {
+      id: 1,
+      name: "João Alfredo",
+    },
+    game: {
+      id: 1,
+      name: "Banco Imobiliário",
+    },
+  },
+];
 
 // export async function createRental(req, res) {}
 
